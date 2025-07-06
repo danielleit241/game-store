0c23d6ebc90d55a86380028a6e8bc1fa063cb07f
danielleit241 <danielleee241@gmail.com>
2025-07-06 10:07:48 +0700

    Add project files.

---
+++ GameStore.Frontend/Clients/GameClient.cs
@@ -0,0 +1,57 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients
+{
+    public class GameClient
+    {
+        private readonly List<GameSummary?> games =
+        [
+            new(){
+                Id = 1,
+                Name = "The Witcher 3: Wild Hunt",
+                Genre = "RPG",
+                Price = 49.99m,
+                ReseleaseDate = new DateOnly(2015, 5, 19)
+            },
+            new(){
+                Id = 2,
+                Name = "DOOM Eternal",
+                Genre = "FPS",
+                Price = 29.99m,
+                ReseleaseDate = new DateOnly(2020, 3, 20)
+            },
+            new(){
+                Id = 3,
+                Name = "GTA V",
+                Genre = "Action",
+                Price = 30.00m,
+                ReseleaseDate = new DateOnly(2020, 3, 24)
+            }
+        ];
+
+        private readonly Genre[] genres = new GenresClient().GetGenres();
+
+        public GameSummary?[] GetGames()
+        {
+            return [.. games];
+        }
+
+        public void AddGame(GameDetails game)
+        {
+            var genre = genres.ToList().FirstOrDefault(g => g.Id == int.Parse(game.GenreId!));
+            if (genre == null)
+            {
+                throw new ArgumentException("Invalid genre ID");
+            }
+            var newGame = new GameSummary
+            {
+                Id = games.Count + 1,
+                Name = game.Name,
+                Genre = genre.Name,
+                Price = game.Price,
+                ReseleaseDate = game.ReseleaseDate
+            };
+            games.Add(newGame);
+        }
+    }
+}
---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,23 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients
+{
+    public class GenresClient
+    {
+        private readonly Genre[] genres =
+        [
+            new Genre { Id = 1, Name = "Action" },
+            new Genre { Id = 2, Name = "Adventure" },
+            new Genre { Id = 3, Name = "Role-Playing" },
+            new Genre { Id = 4, Name = "Simulation" },
+            new Genre { Id = 5, Name = "Strategy" },
+            new Genre { Id = 6, Name = "Sports" },
+            new Genre { Id = 7, Name = "Puzzle" }
+        ];
+
+        public Genre[] GetGenres()
+        {
+            return genres;
+        }
+    }
+}
